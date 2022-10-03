3b6043975af9f0452b131694e9d12f9f919c63e9
Rober Martinez <rober.martinez@sofka.com.co>
2022-10-03 12:43:06 -0500

    actualizacion a capacitor 3

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
-
   constructor() {}
 
+  tab1() {
+    console.log('Tab 1');
+  }
+
+  tab2() {
+    console.log('Tab 2');
+  }
+
+  tab3() {
+    console.log('Tab 3');
+  }
 }
